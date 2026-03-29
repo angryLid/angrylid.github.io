96082fc32fcf52e781389c3675da802e079f0f73
angrylid <zm4399@gmail.com>
2026-03-29 16:59:52 +0800

    support formula

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import remarkGithubAdmonitions from 'remark-github-blockquote-alert';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
 
   markdown: {
-      remarkPlugins: [remarkGithubAdmonitions],
+      remarkPlugins: [remarkGithubAdmonitions, remarkMath],
+      rehypePlugins: [rehypeKatex],
   },
 
   integrations: [mdx()],
